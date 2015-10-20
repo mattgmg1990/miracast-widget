c5712743f5355f229b12acfee9a9678b64c16589
Matt Garnes <matt@mattgarnes.com>
2015-10-19 22:58:18 -0700

    Revert "Fail gracefully when denied Samsung ALLCAST permission."
    This reverts commit b89ad1cd3e878b657a47fcd36c78a2346ff741bb.

--- src/com/mattgmg/miracastwidget/MainActivity.java
+++ src/com/mattgmg/miracastwidget/MainActivity.java
@@ -8,7 +8,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.util.Log;
 import android.view.Menu;
 import android.widget.Toast;
 
@@ -16,24 +15,11 @@ public class MainActivity extends Activity {
 	public static final String ACTION_WIFI_DISPLAY_SETTINGS = "android.settings.WIFI_DISPLAY_SETTINGS";
     public static final String ACTION_CAST_SETTINGS = "android.settings.CAST_SETTINGS";
     public static final String SETTINGS_APP_PACKAGE_NAME = "com.android.settings";
-    public static final String SAMSUNG_ALL_CAST_PERMISSION
-            = "com.android.setting.permission.ALLSHARE_CAST_SERVICE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-        // Check for Samsung permission. If it is defined, but not granted (as it is on older
-        // devices), then we can't launch the proper Settings Activity.
-        if (isPermissionDefined(SAMSUNG_ALL_CAST_PERMISSION)
-                && !isPermissionGranted(SAMSUNG_ALL_CAST_PERMISSION)) {
-            // Show different message or tutorial if Samsung?
-            showErrorToast();
-            finish();
-            return;
-        }
-
         Intent wifiActionIntent = new Intent(ACTION_WIFI_DISPLAY_SETTINGS);
         wifiActionIntent.setPackage(SETTINGS_APP_PACKAGE_NAME);
         Intent castActionIntent = new Intent(ACTION_CAST_SETTINGS);
@@ -62,36 +48,15 @@ public class MainActivity extends Activity {
     }
 	
 	private boolean isCallable(Intent intent) {
-        List<ResolveInfo> list = getPackageManager().queryIntentActivities(intent,
-                                                                           PackageManager.MATCH_DEFAULT_ONLY);
+        List<ResolveInfo> list = getPackageManager().queryIntentActivities(intent, 
+            PackageManager.MATCH_DEFAULT_ONLY);
         return list.size() > 0;
     }	
 	
     private void startSettingsActivity(Intent intent) {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
-    }
-
-    private boolean isPermissionGranted(String permission) {
-        PackageManager pm = getPackageManager();
-        int hasPerm = pm.checkPermission(permission, getPackageName());
-        if (hasPerm == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private boolean isPermissionDefined(String permission) {
-        PackageManager pm = getPackageManager();
-        try {
-            pm.getPermissionInfo(permission, 0);
-            return true;
-        } catch (PackageManager.NameNotFoundException e) {
-            // Exception thrown, permission not defined.
-            return false;
-        }
-    }
+    } 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
