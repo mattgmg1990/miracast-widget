7a593437cfd9366aed33d3baa97532f1bc76d6ac
Matt Garnes <matt@mattgarnes.com>
2015-10-28 20:59:26 -0700

    Remove hard package name dependency
    - Do not target the launch Intent directly to com.android.settings, for
      some devices that package name is not maintained.
    - Query PackageManager for system applications that match the
      IntentFilters instead of limiting by package name. This ensures that
      third party apps are never launched for the cast settings
      IntentFilter.

--- src/com/mattgmg/miracastwidget/MainActivity.java
+++ src/com/mattgmg/miracastwidget/MainActivity.java
@@ -1,45 +1,59 @@
 package com.mattgmg.miracastwidget;
 
-import java.util.List;
-
-import android.content.ActivityNotFoundException;
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.os.Bundle;
 import android.view.Menu;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends Activity {
 	public static final String ACTION_WIFI_DISPLAY_SETTINGS = "android.settings.WIFI_DISPLAY_SETTINGS";
     public static final String ACTION_CAST_SETTINGS = "android.settings.CAST_SETTINGS";
-    public static final String SETTINGS_APP_PACKAGE_NAME = "com.android.settings";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
         Intent wifiActionIntent = new Intent(ACTION_WIFI_DISPLAY_SETTINGS);
-        wifiActionIntent.setPackage(SETTINGS_APP_PACKAGE_NAME);
         Intent castActionIntent = new Intent(ACTION_CAST_SETTINGS);
-        castActionIntent.setPackage(SETTINGS_APP_PACKAGE_NAME);
-        if(isCallable(wifiActionIntent)){
+
+        ResolveInfo systemResolveInfo = getSystemResolveInfo(wifiActionIntent);
+        if(systemResolveInfo != null){
             try {
-                startSettingsActivity(wifiActionIntent);
+                Intent systemWifiIntent = new Intent();
+                systemWifiIntent.setClassName(systemResolveInfo.activityInfo.applicationInfo.packageName,
+                                    systemResolveInfo.activityInfo.name);
+                startSettingsActivity(systemWifiIntent);
+                finish();
+                return;
             } catch (ActivityNotFoundException exception) {
-                showErrorToast();
+                // We'll show an error below if the next Intent can't be launched
             }
-        } else if(isCallable(castActionIntent)) {
-            try {
-                startSettingsActivity(castActionIntent);
-            } catch (ActivityNotFoundException exception) {
-                showErrorToast();
-            }
-        } else {
-            showErrorToast();
         }
-		finish();
+
+        systemResolveInfo = getSystemResolveInfo(castActionIntent);
+        if(systemResolveInfo != null) {
+            try {
+                Intent systemCastIntent = new Intent();
+                systemCastIntent.setClassName(systemResolveInfo.activityInfo.applicationInfo.packageName,
+                                    systemResolveInfo.activityInfo.name);
+                startSettingsActivity(systemCastIntent);
+                finish();
+                return;
+            } catch (ActivityNotFoundException exception) {
+                // Show an error in the block below.
+            }
+        }
+
+        // Show an error and fail
+        showErrorToast();
+        finish();
 	}
 
     private void showErrorToast() {
@@ -47,11 +61,22 @@ public class MainActivity extends Activity {
         Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
     }
 	
-	private boolean isCallable(Intent intent) {
-        List<ResolveInfo> list = getPackageManager().queryIntentActivities(intent,
-                                                                           PackageManager.MATCH_DEFAULT_ONLY);
-        return list.size() > 0;
-    }	
+    private ResolveInfo getSystemResolveInfo(Intent intent) {
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> list = pm.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        for (ResolveInfo info : list) {
+            try {
+                ApplicationInfo activityInfo = pm.getApplicationInfo(info.activityInfo.packageName,
+                                                                   0);
+                if ((activityInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
+                    return info;
+                }
+            } catch (PackageManager.NameNotFoundException e) {
+                // Continue to next ResolveInfo
+            }
+        }
+        return null;
+    }
 	
     private void startSettingsActivity(Intent intent) {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
